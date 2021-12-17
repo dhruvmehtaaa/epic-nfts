f52df4e33c8ad6e267a5e6d5c5f4a574bc5bddc5
Dhruv Mehta <64071840+dhruvmehtaaa@users.noreply.github.com>
2021-12-17 10:47:24 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,3 @@
-/*require('@nomiclabs/hardhat-waffle');
-
-module.exports = {
-  solidity: '0.8.0',
-  networks: {
-    rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/Ae8Bo8vWhzR5JXfkR6ws82arcb-4CwZB',
-      accounts: ['e6612c7357cce20259e60f50b4c0a0dad2c965e0b3b25b2daf7d93f2620dbeaa'],
-    },
-  },
-};*/
-
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
